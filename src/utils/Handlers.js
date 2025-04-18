d44fcec67c2aed03d121acdaee52e83c1529d392
Jay200123 <kenken.ayonon@gmail.com>
2025-04-18 20:24:11 +0800

    Add ErrorHandler and SuccessHandler for standardized response handling

---
+++ src/utils/Handlers.js
@@ -0,0 +1,16 @@
+export class ErrorHandler extends Error {
+  constructor(statusCode, message) {
+    super(message);
+    this.statusCode = statusCode;
+    this.message = message;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export const SuccessHandler = (res, statusCode, data, message) => {
+  return res.status(statusCode).json({
+    status: statusCode,
+    data: data,
+    message: message,
+  });
+};
