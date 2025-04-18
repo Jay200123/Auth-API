3550853cfa242e5a7573c60c9eaf1f52943b4364
Jay200123 <kenken.ayonon@gmail.com>
2025-04-18 20:23:11 +0800

    Add authentication routes with login handler

---
+++ src/routes/auth/route.js
@@ -0,0 +1,26 @@
+import User from "../users/model.js";
+import { AuthService } from "./service.js";
+import { JWTMiddleware } from "../../middleware/index.js";
+import { AuthController } from "./controller.js";
+import express from "express";
+
+const router = express.Router();
+
+const jwtMiddleware = new JWTMiddleware();  
+const authService = new AuthService(User, jwtMiddleware);
+const authController = new AuthController(authService);
+
+const authRoutes = [
+  {
+    method: "post",
+    path: "/login",
+    handler: authController.login.bind(authController),
+  },
+];
+
+authRoutes.forEach((route) => {
+  const { method, path, handler } = route;
+  router[method](path, handler);
+});
+
+export default router;
