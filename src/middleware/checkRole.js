df6caf8a96b2b4de1c13b6d7ee606ec1be3abde8
Jay200123 <kenken.ayonon@gmail.com>
2025-04-19 12:23:51 +0800

    Enhance userRole method documentation in RoleMiddleware for clarity

--- src/middleware/checkRole.js
+++ src/middleware/checkRole.js
@@ -10,12 +10,32 @@ export class RoleMiddleware extends TokenMiddleware {
     super(new JWTMiddleware());
   }
 
+  /**
+   *
+   * @param  {...any} roles - The roles to check against the user's roles.
+   * @returns
+   */
   userRole(...roles) {
     return async (req, res, next) => {
       try {
+        /**
+         * Check if the user is authenticated and has the required role.
+         * If the user is not authenticated or does not have the required role,
+         *  return an error.
+         */
         const token = req?.headers["authorization"].split(" ")[1];
 
+        /**
+         * Check if the token is valid and decode it to get the user information.
+         * If the token is invalid, return an error.
+         */
         const decoded = this.JwtMiddleware.ValidateToken(token);
+
+        /**
+         * Check if the user has the required role.
+         * If the user does not have the required role,
+         *  return an error.
+         */
         if (!roles?.includes(decoded?.user?.roles)) {
           return next(new ErrorHandler(401, "Unauthorized"));
         }
