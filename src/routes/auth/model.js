b39c5770401a986bf015869076ad05ef8c19acb2
Jay200123 <kenken.ayonon@gmail.com>
2025-04-19 11:55:20 +0800

    Add Token model with schema for access tokens

---
+++ src/routes/auth/model.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const TokenSchema = new Schema({
+  access_token: {
+    type: String,
+    required: true,
+  },
+  date_created: {
+    type: Date,
+    default: Date.now,
+  },
+  date_modified: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Token = mongoose.model("token", TokenSchema);
+export default Token;
