fec44b45cdecde24b7a489a33bf630d241689dee
Jay200123 <kenken.ayonon@gmail.com>
2025-04-19 12:23:57 +0800

    Refactor AuthService initialization to include Token model for improved token management

--- src/routes/auth/route.js
+++ src/routes/auth/route.js
@@ -1,13 +1,13 @@
 import User from "../users/model.js";
+import Token from "./model.js";
 import { AuthService } from "./service.js";
 import { JWTMiddleware } from "../../middleware/index.js";
 import { AuthController } from "./controller.js";
 import express from "express";
 
 const router = express.Router();
-
-const jwtMiddleware = new JWTMiddleware();  
-const authService = new AuthService(User, jwtMiddleware);
+ 
+const authService = new AuthService(User, Token, new JWTMiddleware());
 const authController = new AuthController(authService);
 
 const authRoutes = [
