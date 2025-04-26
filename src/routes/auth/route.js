b6a6e33be0cae112d6e599af69ff86f7c2604ee6
Jay200123 <kenken.ayonon@gmail.com>
2025-04-26 17:21:56 +0800

    Added role, middleware properties and new Refresh Token Routes

--- src/routes/auth/route.js
+++ src/routes/auth/route.js
@@ -2,10 +2,14 @@ import User from "../users/model.js";
 import UserDetails from "../user_details/model.js";
 import Token from "./model.js";
 import { AuthService } from "./service.js";
-import { JWTMiddleware } from "../../middleware/index.js";
+import {
+  JWTMiddleware,
+  TokenMiddleware,
+  RoleMiddleware,
+} from "../../middleware/index.js";
 import { AuthController } from "./controller.js";
 import express from "express";
-import { METHOD, PATH } from "../../constants/index.js";
+import { METHOD, PATH, ROLE } from "../../constants/index.js";
 
 const router = express.Router();
 
@@ -16,28 +20,43 @@ const authService = new AuthService(
   new JWTMiddleware()
 );
 const authController = new AuthController(authService);
+const tokenMiddleware = new TokenMiddleware(new JWTMiddleware(), User, Token);
+const roleMiddleware = new RoleMiddleware();
 
 const authRoutes = [
   {
     method: METHOD.GET,
     path: PATH.REGISTER,
+    role: [],
+    middleware: [],
     handler: authController.registerUser.bind(authController),
   },
   {
     method: METHOD.POST,
     path: PATH.LOGIN,
+    role: [],
+    middleware: [],
     handler: authController.login.bind(authController),
+  },
+  {
+    method: METHOD.GET,
+    path: PATH.REFRESH_TOKEN,
+    role: [ROLE.ADMIN, ROLE.USER],
+    middleware: [tokenMiddleware.verifyAccessToken.bind(tokenMiddleware)],
+    handler: authController.refreshToken.bind(authController),
   },
   {
     method: METHOD.POST,
     path: PATH.LOGOUT,
+    role: [],
+    middleware: [],
     handler: authController.logout.bind(authController),
   },
 ];
 
 authRoutes.forEach((route) => {
-  const { method, path, handler } = route;
-  router[method](path, handler);
+  const { method, path, role = [], middleware = [], handler } = route;
+  router[method](path, middleware, roleMiddleware.userRole(...role), handler);
 });
 
 export default router;
